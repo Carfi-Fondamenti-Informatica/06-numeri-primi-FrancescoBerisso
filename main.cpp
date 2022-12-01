9e7ec6e91f02b0431b87b16c077ae89cca233225
FrancescoBerisso <115414198+FrancescoBerisso@users.noreply.github.com>
2022-12-01 12:08:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include "lib.h"
 int main(){
   int a = 0, i = 2;
-    cin >> a;
-    if(n_primo(a, i)){
-        cout << "numero primo";
-    }
-    else{
-        cout << "numero non primo";
-    }
+  cin >> a;
+  if(n_primo(a, i)){
+      cout << "numero primo";
+  }
+  else{
+      cout << "numero non primo";
+  }
   return 0;
 }
