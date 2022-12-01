101de90a2db829dfb20ce72c5f38f56dd12eac62
FrancescoBerisso <115414198+FrancescoBerisso@users.noreply.github.com>
2022-12-01 12:05:03 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include "lib.h"
+bool n_primo(int a, int i){
+    if(a % i == 0){
+        if(a == i){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    n_primo(a, i + 1);
+}
